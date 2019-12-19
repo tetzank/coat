f954a1068e60d754c299b0d41b78c80496afd94c
tetzank <tetzank@users.sourceforge.net>
2019-12-19 17:50:53 +0100

    added examples for calling functions and reading data structures

---
+++ examples/call.cpp
@@ -0,0 +1,59 @@
+#include <cstdio>
+#include <vector>
+#include <numeric>
+
+#include <coat/Function.h>
+#include <coat/ControlFlow.h>
+
+
+// function to be called, residing outside of generated code
+int calc(int value){
+	return value * 2;
+}
+
+int main(){
+	// signature of the generated function
+	using func_t = int (*)(int value);
+
+#ifdef ENABLE_ASMJIT
+	// initialize backend, AsmJit in this case
+	coat::runtimeasmjit asmrt;
+	// context object representing the generated function
+	coat::Function<coat::runtimeasmjit,func_t> fn(asmrt);
+#elif defined(ENABLE_LLVMJIT)
+	// initialize LLVM backend
+	coat::runtimellvmjit::initTarget();
+	coat::runtimellvmjit llvmrt;
+	// context object representing the generated function
+	coat::Function<coat::runtimellvmjit,func_t> fn(llvmrt);
+#else
+#	error "Neither AsmJit nor LLVM enabled"
+#endif
+
+	// start of the EDSL code describing the code of the generated function
+	{
+		// get function arguments as "meta-variables"
+		auto [value] = fn.getArguments("value");
+		
+		// generate function call
+		auto ret = coat::FunctionCall(fn, calc, "calc", value);
+
+		// specify return value
+		coat::ret(fn, ret);
+	}
+	// finalize code generation and get function pointer to the generated function
+	func_t foo = fn.finalize();
+
+	// execute the generated function
+	uint64_t result = foo(42);
+
+	// print result
+	uint64_t expected = calc(42);
+	if(result == expected){
+		printf("correct result: %lu\n", result);
+		return 0;
+	}else{
+		printf("wrong result:\nresult: %lu; expected: %lu\n", result, expected);
+		return -1;
+	}
+}
---
+++ examples/reading_datastruct.cpp
@@ -0,0 +1,89 @@
+#include <cstdio>
+#include <vector>
+#include <numeric>
+
+#include <coat/Function.h>
+#include <coat/ControlFlow.h>
+
+
+class my_vector {
+// declare all member variables: int *start, *finish, *capacity;
+// macro adds metadata to calculate
+// data layout at compile-time
+#define MEMBERS(x)    \
+    x(int*, start)    \
+    x(int*, finish)   \
+    x(int*, capacity)
+
+DECLARE_PRIVATE(MEMBERS)
+#undef MEMBERS
+
+public:
+	my_vector(){
+		// set it to some hardcoded state
+		start = new int[42];
+		finish = start + 23;
+		capacity = start + 42;
+	}
+	~my_vector(){
+		delete[] start;
+	}
+
+	size_t size() const {
+		return finish - start;
+	}
+};
+
+
+
+int main(){
+	// signature of the generated function: taking pointer to data structure
+	using func_t = size_t (*)(my_vector *vec);
+
+#ifdef ENABLE_ASMJIT
+	// initialize backend, AsmJit in this case
+	coat::runtimeasmjit asmrt;
+	// context object representing the generated function
+	coat::Function<coat::runtimeasmjit,func_t> fn(asmrt);
+#elif defined(ENABLE_LLVMJIT)
+	// initialize LLVM backend
+	coat::runtimellvmjit::initTarget();
+	coat::runtimellvmjit llvmrt;
+	// context object representing the generated function
+	coat::Function<coat::runtimellvmjit,func_t> fn(llvmrt);
+#else
+#	error "Neither AsmJit nor LLVM enabled"
+#endif
+
+	// start of the EDSL code describing the code of the generated function
+	{
+		// get function arguments as "meta-variables"
+		auto [vec] = fn.getArguments("vec");
+
+		// read member "start"
+		auto start = vec.get_value<my_vector::member_start>();
+		// read member "finish"
+		auto finish = vec.get_value<my_vector::member_finish>();
+		// calculate number of elements between both pointers
+		auto size = finish - start;
+
+		// specify return value
+		coat::ret(fn, size);
+	}
+	// finalize code generation and get function pointer to the generated function
+	func_t foo = fn.finalize();
+
+	my_vector vec;
+	// execute the generated function
+	size_t result = foo(&vec);
+
+	// print result
+	size_t expected = vec.size();
+	if(result == expected){
+		printf("correct result: %lu\n", result);
+		return 0;
+	}else{
+		printf("wrong result:\nresult: %lu; expected: %lu\n", result, expected);
+		return -1;
+	}
+}
